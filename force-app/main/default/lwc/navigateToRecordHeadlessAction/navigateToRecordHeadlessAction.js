f71965fe927d54d2cfb6d699e772ea704962829c
MustafaStretch <167743065+MustafaStretch@users.noreply.github.com>
2024-04-23 19:18:08 +0200

    commit

--- force-app/main/default/lwc/navigateToRecordHeadlessAction/navigateToRecordHeadlessAction.js
+++ force-app/main/default/lwc/navigateToRecordHeadlessAction/navigateToRecordHeadlessAction.js
@@ -1,16 +1,28 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import { getRecord } from "lightning/uiRecordApi";
 
+  
 export default class NavigateToRecordHeadlessAction extends NavigationMixin(
     LightningElement
 ) {
+    @api recordId;
+    @api objectApiName;
     @api invoke() {
-        this[NavigationMixin.Navigate]({
-            type: 'standard__objectPage',
+        const config = {
+            type: 'standard__webPage',
             attributes: {
-                objectApiName: 'Contact',
-                actionName: 'home'
+                url: this.urlField
             }
-        });
-    }
+            
+        }
+        this[NavigationMixin.Navigate](config);
+
+    }   
+    
+    get urlField() {
+        return objectApiName + ".PortalURL__c"
+      }
+      @wire(getRecord, { recordId: "$recordId", fields: [this.urlField] }) record;
 }
+
